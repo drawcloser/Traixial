8eff335b560cbc4e40f0d587d631b211427ffe1c
tsouwei <tsouwei11@gmail.com>
2015-02-20 22:12:58 +0800

    traixial

---
+++ mean_and_std_data.R
@@ -0,0 +1,39 @@
+# read files in the UCI HAR Dataset directory
+features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("order", "measurement"), colClasses = c("numeric", "character"))
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("numbers", "activity"), colClasses = c("numeric", "character"))
+
+# read files in the train directory
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "numbers", colClasses = "numeric")
+x_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+
+# read files in the text directory
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "numbers", colClasses = "numeric")
+x_test <- read.table("./UCI HAR Dataset/test/x_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+
+# tidy the train data
+train_activity <- y_train
+for (i in 1:6){
+  train_activity$numbers[train_activity$numbers == i] <- activity_labels[activity_labels$numbers[activity_labels$numbers == i],][,2] 
+}
+train_activity$method <- "train"
+train_data <- cbind(subject_train, train_activity, x_train)
+
+# tidy the test data
+test_activity <- y_test
+for (i in 1:6){
+  test_activity$numbers[test_activity$numbers == i] <- activity_labels[activity_labels$numbers[activity_labels$numbers == i],][,2] 
+}
+test_activity$method <- "test"
+test_data <- cbind(subject_test, test_activity, x_test)
+
+# merge the train_data and test_data
+data <- rbind(train_data, test_data)
+names(data)[4:564] <- features[,"measurement"]
+names(data)[2] <- paste("activity")
+
+# select mean and std of the data
+mean_data <- data[,grep("mean()", names(data), value=T)]
+std_data <- data[,grep("std()", names(data), value=T)]
+mean_and_std_data <- cbind(data[1:3], mean_data, std_data)
